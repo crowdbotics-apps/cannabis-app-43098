e492658e7b24a8b5cb4c9830fa5bff3670ce8231
Crowdbotics <team@crowdbotics.com>
2023-08-27 09:04:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cannabis-app-43098",
+  url: "https://cannabis-app-43098.botics.co",
+  api: "https://cannabis-app-43098.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
